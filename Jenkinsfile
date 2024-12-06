4033c90540d20f6c903449d1ea9a4bf325ee0620
Aravindaraj M <aravinddevopslearning@gmail.com>
2024-12-06 22:30:20 +0530

    jenkins update

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,6 +1,13 @@
 pipeline {
     agent any
 
+    environment {
+        EC2_USER = "ec2-user"
+        EC2_HOST = "54.91.155.225"
+        DOCKER_USERNAME = "aravindmathes"
+        REMOTE_DIR="/home/$EC2_USER/deployment"
+    }
+
     stages {
         stage('Build a Docker Image and Push the image to Docker Hub') {
             steps {
@@ -14,16 +21,27 @@ pipeline {
             }
         stage('Deploy the file in server') {
             steps {
-                withCredentials([
-                    string(credentialsId: 'dockerhub-token', variable: 'DOCKERHUB_TOKEN'),
-                    sshUserPrivateKey(credentialsId: 'devops-project-key', keyFileVariable: 'PEM_FILE')]) 
-                    {
-                        sh """
-                        chmod +x deploy.sh
-                        ./deploy.sh
-                        """
+                script {
+                    withCredentials([
+                        string(credentialsId: 'dockerhub-token', variable: 'DOCKERHUB_TOKEN')
+                    ]) {
+                        sshagent(['devops-project-key']) {
+                            sh """
+                                ssh -o StrictHostKeyChecking=no ${EC2_USER}@${EC2_HOST} << EOF
+                                    echo "Docker Images"
+                                    sudo docker images
+                                EOF
+
+                                chmod +x deploy.sh
+                                ./deploy.sh
+
+                                echo "Copying Docker-compose and its dependencies to EC2 deployment directory."
+                                scp -o StrictHostKeyChecking=no textfile.txt ${EC2_USER}@${EC2_HOST}:${REMOTE_DIR}
+                            """
+                        }
                     }
                 }
             }
         }
+    }
 }
--- deploy.sh
+++ deploy.sh
@@ -3,39 +3,53 @@
 # Exit on any error
 set -e
 
-EC2_USER="ec2-user"
-EC2_HOST="54.91.155.225"
-DOCKER_USERNAME="aravindmathes"
-REMOTE_DIR="/home/$EC2_USER/deployment"
+# EC2_USER="ec2-user"
+# EC2_HOST="54.91.155.225"
+# DOCKER_USERNAME="aravindmathes"
+# REMOTE_DIR="/home/$EC2_USER/deployment"
 
-#Temp file for private key
-echo "$PEM_FILE" | tr -d '\r' > /tmp/devops_project_key.pem
-chmod 600 /tmp/devops_project_key.pem
+# #Temp file for private key
+# echo "$PEM_FILE" | tr -d '\r' > /tmp/devops_project_key.pem
+# chmod 600 /tmp/devops_project_key.pem
 
-echo "private file created"
+# echo "private file created"
 
-cat -e /tmp/devops_project_key.pem
+# cat -e /tmp/devops_project_key.pem
 
-echo "content verification"
-echo "$PEM_FILE" | awk '{print NR ":" $0}'
+# cat -A /tmp/devops_project_key.pem
 
-#login to EC2 Instance to stop running container and do clean up of old deployment files
-ssh -t -i /tmp/devops_project_key.pem "$EC2_USER@$EC2_HOST" << EOF
-  echo "Login to Docker Hub"
-  echo "$DOCKERHUB_TOKEN" | docker login -username "$DOCKER_USERNAME" --password-stdin
-  echo "Stopping and Deleting all the running Docker containers."
-  sudo docker ps -q | xargs -r docker stop
-  sudo docker ps -aq | xargs -r docker rm
-  echo "Deleting all Docker Images."
-  sudo docker images -q | xargs -r docker rmi
-  echo "Docker Images"
-  sudo docker images
-  echo "Logout of Docker Hub"
-EOF
+# echo "content verification"
+# echo "$PEM_FILE" | awk '{print NR ":" $0}'
 
-#copying the docker-compose and its dependent files to EC2 Instance
-echo "Copying Docker-compose and its dependencies to EC2 deployment directory."
-scp -i /tmp/devops_project_key.pem textfile.txt "$EC2_USER@$EC2_HOST:$REMOTE_DIR"
+# #login to EC2 Instance to stop running container and do clean up of old deployment files
+# ssh -t -i /tmp/devops_project_key.pem "$EC2_USER@$EC2_HOST" << EOF
+#   echo "Login to Docker Hub"
+#   echo "$DOCKERHUB_TOKEN" | docker login -username "$DOCKER_USERNAME" --password-stdin
+#   echo "Stopping and Deleting all the running Docker containers."
+#   sudo docker ps -q | xargs -r docker stop
+#   sudo docker ps -aq | xargs -r docker rm
+#   echo "Deleting all Docker Images."
+#   sudo docker images -q | xargs -r docker rmi
+#   echo "Docker Images"
+#   sudo docker images
+#   echo "Logout of Docker Hub"
+# EOF
 
-#Clean up temp private key file
-rm /tmp/devops_project_key.pem
+# #copying the docker-compose and its dependent files to EC2 Instance
+# echo "Copying Docker-compose and its dependencies to EC2 deployment directory."
+# scp -i /tmp/devops_project_key.pem textfile.txt "$EC2_USER@$EC2_HOST:$REMOTE_DIR"
+
+# #Clean up temp private key file
+# rm /tmp/devops_project_key.pem
+
+echo "$DOCKERHUB_TOKEN" | docker login -username "$DOCKER_USERNAME" --password-stdin
+echo "Stopping and Deleting all the running Docker containers."
+docker ps -q | xargs -r docker stop
+docker ps -aq | xargs -r docker rm
+echo "Deleting all Docker Images."
+docker images -q | xargs -r docker rmi
+echo "Cleaning up old files in $REMOTE_DIR."
+sudo rm -rf $REMOTE_DIR
+echo "Recreating the directory $REMOTE_DIR."
+mkdir -p $REMOTE_DIR
+chmod 755 $REMOTE_DIR
