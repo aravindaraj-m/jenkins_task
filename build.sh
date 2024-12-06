15e702c790a3dca613d14e1069fae106bf060763
Aravindaraj M <aravinddevopslearning@gmail.com>
2024-12-06 14:12:55 +0530

    jenkins update

--- build.sh
+++ build.sh
@@ -10,10 +10,10 @@ echo "logining into docker hub..."
 echo "$DOCKERHUB_TOKEN" | docker login -u "$DOCKER_USERNAME" --password-stdin
 
 echo "List of docker images created"
-sudo docker images
+docker images
 
 echo "List of docker containers"
-sudo docker ps -a
+docker ps -a
 
 echo "Logout of Docker Hub"
-suco docker logout
+docker logout
