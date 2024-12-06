00555646281c2079abad3f0d9f723b46aefe3259
Aravindaraj M <aravinddevopslearning@gmail.com>
2024-12-06 18:16:11 +0530

    jenkins update

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,7 @@ DOCKER_USERNAME="aravindmathes"
 REMOTE_DIR="/home/$EC2_USER/deployment"
 
 #login to EC2 Instance to stop running container and do clean up of old deployment files
-ssh -i "$PEM_FILE" "$EC2_USER@$EC2_HOST" << EOF
+ssh -o StrictHostKeyChecking=no "$EC2_USER@$EC2_HOST" << EOF
   echo "Login to Docker Hub"
   echo "$DOCKERHUB_TOKEN" | docker login -username "$DOCKER_USERNAME" --password-stdin
   echo "Stopping and Deleting all the running Docker containers."
@@ -22,4 +22,4 @@ EOF
 
 #copying the docker-compose and its dependent files to EC2 Instance
 echo "Copying Docker-compose and its dependencies to EC2 deployment directory."
-scp -i "$PEM_FILE" textfile.txt "$EC2_USER@$EC2_HOST:$REMOTE_DIR"
+scp -o StrictHostKeyChecking=no textfile.txt "$EC2_USER@$EC2_HOST:$REMOTE_DIR"
